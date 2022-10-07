96a8bc985ef8090bbce1f9f128e2967445e0d078
Arjun <arjunjot18@gmail.com>
2022-10-07 18:15:44 +0200

    1

---
+++ app/src/main/java/com/example/twoactivities/Activity2.java
@@ -0,0 +1,20 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+}
--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    //----------------------------------------------------------------------------------------------------
+    //wrong path to get string from text feild
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.message";//error here <----------------
+    //----------------------------------------------------------------------------------------------------
+    private Button btnSend;
+    private EditText mMessageEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSend =  findViewById(R.id.send);
+        btnSend.setOnClickListener(view -> openActivityTwo());
+
+
     }
+    public void openActivityTwo(){
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, Activity2.class);
+        mMessageEditText = findViewById(R.id.editText_main);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(mMessageEditText.getText().toString(), message);
+        startActivity(intent);
+    }
+
 }
